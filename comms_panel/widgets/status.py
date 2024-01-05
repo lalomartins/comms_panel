a482270a553270dd78a2910ef2c4a38a6246f1b7
Lalo Martins <lalo.martins@gmail.com>
2024-01-06 04:10:59 +0900

    separate widget for status

---
+++ comms_panel/widgets/status.py
@@ -0,0 +1,46 @@
+import wx, wx.html
+
+from comms_panel.lib.html import cleanup_html
+
+
+class StatusPanel(wx.Panel):
+    status: dict
+
+    def __init__(self, parent, status, width, *args, **kw):
+        super().__init__(*args, parent, **kw)
+        self.status = status
+        self.base_size = wx.Size(width, 30)
+        self.panel_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self.panel_sizer)
+        self.SetAutoLayout(1)
+        self.panel_sizer.Fit(self)
+        self.head = wx.StaticText(self)
+        self.body = wx.html.HtmlWindow(self, style=wx.html.HW_SCROLLBAR_AUTO, size=self.base_size)
+        self.orig_head = wx.StaticText(self)
+        self.orig_body = wx.html.HtmlWindow(self, style=wx.html.HW_SCROLLBAR_AUTO, size=self.base_size)
+        self.panel_sizer.AddMany([self.head, self.body, self.orig_head, self.orig_body])
+        self.update()
+
+    def update(self) -> None:
+        self.head.SetLabel(f"{self.status["created_at"]} by {self.status["account"]["display_name"]}")
+        orig = self.status.get("reblog", {})
+
+        self.body.Show()
+        if self.status["content"]:
+            self.body.SetPage(cleanup_html(self.status["content"]))
+            self.body.SetMinSize(wx.Size(self.base_size.width, self.body.VirtualSize.height + 10))
+        elif orig:
+            self.body.Hide()
+        else:
+            self.body.SetPage("<i>No content</i>")
+            self.body.SetMinSize(wx.Size(self.base_size.width, 20))
+
+        if orig:
+            self.orig_head.SetLabel(f"Reblog of {orig["created_at"]} by {orig["account"]["display_name"]}")
+            self.orig_body.SetPage(cleanup_html(orig["content"]))
+            self.orig_head.Show()
+            self.orig_body.SetMinSize(wx.Size(self.base_size.width, self.orig_body.VirtualSize.height + 10))
+            self.orig_body.Show()
+        else:
+            self.orig_head.Hide()
+            self.orig_body.Hide()
--- comms_panel/widgets/timeline.py
+++ comms_panel/widgets/timeline.py
@@ -5,6 +5,7 @@ import wx, wx.lib.scrolledpanel, wx.html
 
 from comms_panel.backends.timeline import Timeline
 from comms_panel.lib.html import cleanup_html
+from comms_panel.widgets.status import StatusPanel
 
 
 class TimelinePanel(wx.lib.scrolledpanel.ScrolledPanel):
@@ -16,41 +17,25 @@ class TimelinePanel(wx.lib.scrolledpanel.ScrolledPanel):
         self.timeline = timeline
         self.panel_sizer = wx.FlexGridSizer(1)
         self.SetSizer(self.panel_sizer)
-        # self.SetAutoLayout(1)
-        # self.panel_sizer.Fit(self)
         self.EnableScrolling(False, True)
         self.panel_sizer.Add(wx.StaticText(self, label=timeline.title))
         self.SetupScrolling()
 
     def update(self):
         self.timeline.update()
-        size = wx.Size((self.Size.width or self._default_width) - 20, 30)
+        width = (self.Size.width or self._default_width) - 20
         self.panel_sizer.Clear(delete_windows=True)
         self.panel_sizer.Add(wx.StaticText(self, label=self.timeline.title))
         for status in self.timeline.statuses:
             self.panel_sizer.AddSpacer(20)
-            self.panel_sizer.Add(
-                wx.StaticText(self, label=f"{status["created_at"]} by {status["account"]["display_name"]}")
-            )
-            html = wx.html.HtmlWindow(self, style=wx.html.HW_SCROLLBAR_AUTO, size=size)
-            orig = status.get("reblog", {})
-            if status["content"]:
-                html.SetPage(cleanup_html(status["content"]))
-            elif not orig:
-                self.panel_sizer.Add(wx.StaticText(self, label="No content"))
-
-            if orig:
-                self.panel_sizer.Add(
-                    wx.StaticText(self, label=f"Reblog of {orig["created_at"]} by {orig["account"]["display_name"]}")
-                )
-                html.SetPage(cleanup_html(orig["content"]))
-            html.SetMinSize(wx.Size(size.width, html.VirtualSize.height + 10))
-            self.panel_sizer.Add(html)
+            self.panel_sizer.Add(StatusPanel(self, status, width))
         self.Layout()
         self.SetupScrolling()
 
     async def auto_update(self) -> None:
         while self.timeline.next_update is not None:
-            await asyncio.sleep((self.timeline.next_update - datetime.now()).total_seconds())
+            await asyncio.sleep(
+                (self.timeline.next_update - datetime.now()).total_seconds()
+            )
             debug(f"Updating {self.timeline.title} timeline")
             self.update()
